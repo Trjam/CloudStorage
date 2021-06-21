e53f7d467a3c42287521e49cab545c53e3678fc4
Trjamich <Alien.trjam@gmail.com>
2021-06-21 10:15:10 +0300

    Lesson_1

--- src/main/java/com/polozov/cloudstorage/lesson01/ClientHandler.java
+++ src/main/java/com/polozov/cloudstorage/lesson01/ClientHandler.java
@@ -1,9 +1,6 @@
 package com.polozov.cloudstorage.lesson01;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
@@ -23,39 +20,78 @@ public class ClientHandler implements Runnable {
             System.out.printf("Client %s connected\n", socket.getInetAddress());
             while (true) {
                 String command = in.readUTF();
+
+                //Вывод команды перенес выше и добавил вывод имени файла, чтоб в консоли упорядочено хоть было, чисто удобства ради
+                System.out.println(command);
+
                 if ("upload".equals(command)) {
                     try {
-                        File file = new File("server" + File.separator + in.readUTF());
+                        String filename = in.readUTF();
+                        System.out.println(filename);
+
+                        File file = new File("server" + File.separator + filename);
                         if (!file.exists()) {
                             file.createNewFile();
                         }
+
                         FileOutputStream fos = new FileOutputStream(file);
 
                         long size = in.readLong();
-
                         byte[] buffer = new byte[8 * 1024];
 
                         for (int i = 0; i < (size + (buffer.length - 1)) / (buffer.length); i++) {
                             int read = in.read(buffer);
                             fos.write(buffer, 0, read);
                         }
+
                         fos.close();
+
                         out.writeUTF("OK");
+                        System.out.println("receiving status: OK");
+
                     } catch (Exception e) {
                         out.writeUTF("FATAL ERROR");
                     }
-                }
 
-                if ("download".equals(command)) {
+                } else if ("download".equals(command)) {
                     // TODO: 14.06.2021
-                }
-                if ("exit".equals(command)) {
+                    try {
+                        String filename = in.readUTF();
+                        System.out.println(filename);
+                        File file = new File("server" + File.separator + filename);
+                        if (!file.exists()) {
+                            throw new FileNotFoundException();
+                        }
+
+                        long fileLength = file.length();
+                        FileInputStream fis = new FileInputStream(file);
+
+                        out.writeLong(fileLength);
+
+                        int read = 0;
+                        byte[] buffer = new byte[8 * 1024];
+                        while ((read = fis.read(buffer)) != -1) {
+                            out.write(buffer, 0, read);
+                        }
+
+                        out.flush();
+
+                        String status = in.readUTF();
+                        System.out.println("sending status: " + status);
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                } else if ("exit".equals(command)) {
                     System.out.printf("Client %s disconnected correctly\n", socket.getInetAddress());
                     break;
-                }
 
-                System.out.println(command);
-                out.writeUTF(command);
+                } else {
+                    out.writeUTF(command);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/polozov/cloudstorage/lesson01/Server.java
+++ src/main/java/com/polozov/cloudstorage/lesson01/Server.java
@@ -7,7 +7,10 @@ import java.util.concurrent.Executors;
 public class Server {
     // TODO: 14.06.2021
     // организовать корректный вывод статуса
+	// c else норм приходит, точнее не уходит эхо, когда не надо
+
     // подумать почему так реализован цикл в ClientHandler
+	// ибо делим файл на размер буфера, получаем количество циклов, сколько раз надо прочитать полный буфер + 1 проход на не полный буфер
     public Server() {
         ExecutorService service = Executors.newFixedThreadPool(4);
         try (ServerSocket server = new ServerSocket(5678)) {
